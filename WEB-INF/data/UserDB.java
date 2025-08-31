8200e7d1160c183e01aad8211719003a8784c6f3
User-Name-netizen <thanhnguyen108.online.2021@gmail.com>
2025-09-01 00:42:34 +0700

    update

--- WEB-INF/data/UserDB.java
+++
@@ -1,7 +0,0 @@
-package murach.data;
-
-public class UserDB {
-    public static void insert(murach.business.User user) {
-        System.out.println("Inserted: " + user.getEmail()); // Giả lập
-    }
-}
